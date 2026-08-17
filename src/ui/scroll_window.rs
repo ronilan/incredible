33641a6f936290d118000024e915abb48f600054
Ron Ilan <ronilan.webdev@gmail.com>
2026-08-16 18:18:43 -0700

    Responsive layout. Better mobile reading. Logo truncated.

--- src/ui/scroll_window.rs
+++ src/ui/scroll_window.rs
@@ -1,5 +1,6 @@
 use incredible::*;
 use incredible_elements::{FrameKind, ScrollArea};
+use incredible_elements_extra::Markdown;
 use incredible_helpers_layout::*;
 
 use crate::state::State;
@@ -15,6 +16,10 @@ pub fn build_scroll_window() -> ScrollArea<State> {
     scroll_window.on_window(|el, _state, event| {
         if event.window == Window::Resize {
             el.width(Platform::columns()).height(Platform::rows());
+
+            if let Some(md) = el.elements.cot::<Markdown<State>>().first() {
+                md.wrap_at(el.get_width().saturating_sub(4).min(80));
+            }
 
             el.elements_snap_top()
                 .elements_to_center_x()
